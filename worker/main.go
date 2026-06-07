586cbd09b1e2a230aa4ed8274a1e039e5c28c969
Maxim-Belyi <btf9458@gmail.com>
2026-06-07 12:46:27 +0500

    refactor(worker): worker main.go

--- worker/main.go
+++ worker/main.go
@@ -2,20 +2,15 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"log"
-	"math/rand"
 	"os"
-	"time"
+
+	"pricetracker/worker/internal/repository"
+	"pricetracker/worker/internal/service"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
-
-type Task struct {
-	Id  int    `json:"id"`
-	Url string `json:"url"`
-}
 
 func main() {
 	dsn := os.Getenv("DB_DSN")
@@ -66,6 +61,9 @@ func main() {
 
 	log.Printf("Очередь объявлена! Имя: %s, Сообщений: %d", q.Name, q.Messages)
 
+	itemRepo := repository.NewItemRepository(db)
+	itemService := service.NewItemService(itemRepo)
+
 	msgs, err := ch.Consume(
 		"parsing_tasks",
 		"",
@@ -84,34 +82,12 @@ func main() {
 	go func() {
 		for msg := range msgs {
 			log.Printf("Получено сообщение: %s", msg.Body)
-
-			var t Task
-
-			if err := json.Unmarshal(msg.Body, &t); err != nil {
+			if err := itemService.ProcessTask(msg.Body); err != nil {
 				log.Printf("Ошибка декодирования Json: %v", err)
-				msg.Nack(false, false)
-				continue
-			}
-
-			log.Printf("Начинаю парсинг для URL: %s", t.Url)
-			time.Sleep(2 * time.Second)
-			price := float64(rand.Intn(1000)) + 100.00
-
-			query := `
-			UPDATE items SET current_price = $1, 
-			status = 'processed',
-			updated_at = CURRENT_TIMESTAMP WHERE id = $2
-			`
-
-			if _, err := db.Exec(query, price, t.Id); err != nil {
-				log.Printf("Ошибка обновления БД: %v", err)
 				msg.Nack(false, true)
 				continue
-			}
-
+			}			
 			msg.Ack(false)
-			log.Printf("Успешно! Товар ID %d получил цену %.2f", t.Id, price)
-
 		}
 	}()
 	log.Println("Worker запущен! Ожидание сообщений...")
